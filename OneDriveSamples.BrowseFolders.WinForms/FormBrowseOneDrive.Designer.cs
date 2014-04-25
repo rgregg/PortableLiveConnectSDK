fae5e2f5cab76e88cdcf4bf7ca09d84d8fb03b83
Ryan Gregg <rgregg@microsoft.com>
2014-04-25 11:58:48 -0700

    Adding new project: OneDrive browser

---
+++ OneDriveSamples.BrowseFolders.WinForms/FormBrowseOneDrive.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace OneDriveSamples.BrowseFolders
+{
+    partial class FormBrowseOneDrive
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormBrowseOneDrive
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 764);
+            this.Name = "FormBrowseOneDrive";
+            this.Text = "OneDrive Browser";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
